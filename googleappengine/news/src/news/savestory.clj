f6b23a58eb2a3fcc766532fcc2f9937df286cd0f
Jeff Foster <jeff.foster@acm.org>
2009-05-12 08:11:56 +0100

    Wire it in

--- googleappengine/news/src/news/savestory.clj
+++ googleappengine/news/src/news/savestory.clj
@@ -3,23 +3,17 @@
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:import (com.google.appengine.api.datastore DatastoreServiceFactory Entity Key Query)))
                                       
-;; We store maps.  Each map has a globally unique ID
-;; Data is immutable to make life easier
-
-(defn- remove-key [m]
-  (remove (fn [x] (= (first x) :key)) m))
-
 (defn store
   [data type]
   (let [entity (Entity. type)]
     (doseq [[k v] data]
       (.setProperty entity k v))
-    (.put (DatastoreServiceFactory/getDatastoreService) entity)))
+    (.put (DatastoreServiceFactory/getDatastoreService) entity)
+    (.getKey entity)))
 
 (defn -doGet                                                  
   [_ request response]
   (ensure-authenticated request response) ;; TODO define a macro to avoid this repetition in every servlet!
   (let [body (.getParameter request "bodytext")
 	title (.getParameter request "title")]    
-    ;; TODO persist this
-    ))
+    (store {:body body :title title} "story")))

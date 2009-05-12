5b5f77749e5b547e9816560249df14a3f5f128f7
Jeff Foster <jeff.foster@acm.org>
2009-05-12 07:49:51 +0100

    Ensure that it actually builds, that's kinda important

--- googleappengine/news/src/news/savestory.clj
+++ googleappengine/news/src/news/savestory.clj
@@ -1,4 +1,5 @@
-(ns news.savestory                                                                        
+(ns news.savestory                                    
+  (:use (news appengine))
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:import (com.google.appengine.api.users User UserService UserServiceFactory))
   (:import (javax.jdo JDOHelper PersistenceManager PersistenceManagerFactory)))
@@ -7,5 +8,6 @@
   [_ request response]
   (ensure-authenticated request response) ;; TODO define a macro to avoid this repetition in every servlet!
   (let [body (.getParameter request "bodytext")
-	title (.getParameter request "title")]
-    ;; TODO persist this))
+	title (.getParameter request "title")]    
+    ;; TODO persist this
+    ))

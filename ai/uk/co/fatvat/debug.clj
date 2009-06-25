98d69171fb7e1892aeb2429a6d1181dbd6b2fc94
Jeff Foster <jeff.foster@acm.org>
2009-06-25 07:02:15 +0100

    Fix up where the doc strings go

--- ai/uk/co/fatvat/debug.clj
+++ ai/uk/co/fatvat/debug.clj
@@ -4,26 +4,26 @@
 (def *dbg-ids* (ref #{:gps}))
 
 (defn dbg 
-  [id format-string & args]
   "Print debugging info if (DEBUG id) has been specified"
+  [id format-string & args]
   (when (contains? id @*dbg-ids*)
     (println (format format-string args))))
 
 (defn debug
-  [& ids]
   "Start dbg output on the given ids"
+  [& ids]
   (dosync
    (alter *dbg-ids* (fn [x] (set (union x ids))))))
 
 (defn undebug
-  [& ids]
   "Stop dbg output on the given ids"
+  [& ids]
   (dosync
    (alter *dbg-ids* (fn [x] (difference x ids)))))
 
 (defn dbg-indent
-  [id indent format-string & args]
   "Print indented debugging info if (DEBUG ID) has been specified"
+  [id indent format-string & args]
   (when (@*dbg-ids* id)
     (println (format (str (apply str (repeat indent \space)) format-string) args))))
 
